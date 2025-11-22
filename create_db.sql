c1f3d55917b4d15b0e55ab9ea1da5797c6d617df
NightFlash96 <nightflash94@gmail.com>
2025-11-22 21:55:55 +0000

    added tables to create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -11,6 +11,23 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    hashedPassword CHAR(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE audit (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	date DATETIME NOT NULL,
+	username VARCHAR(50) NOT NULL,
+	success BOOLEAN NOT NULL,
+    eventType VARCHAR(50) NOT NULL
+);
+
 # Create the application user
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
